941be2ac8aeba3eb25f0fd9b8b519180e556081b
Teighan <tpeterson26@huskers.unl.edu>
2021-08-26 19:51:34 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Teighan Peterson
+ *         tpeterson26@huskers.unl.edu
+ * Date: 2021/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Teighan <3 \n");
+
+  return 0;
+}
